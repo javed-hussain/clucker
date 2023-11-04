df23ab6242979af92348e3aa863e1b3085ea6f2e
javed-hussain <j.hussain1204@gmail.com>
2023-11-04 16:21:52 +0000

    configure admin interface for users

--- microbloggs/admin.py
+++ microbloggs/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+"""configuartion of the admin inyerface for microbloggs"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    '''config of the admin interface for users'''
+    list_display = [
+        'username', 'first_name', 'last_name', 'email', 'is_active', 'bio',
+    ]
